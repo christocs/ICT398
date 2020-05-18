e7edb18e27f4c7d14aa56de6905710e6f8471315
jammy-dodgers <emily@jambox.cc>
2020-05-18 15:31:29 +0800

    Basic FSM with builder

---
+++ src/afk/script/StateMachine.cpp
@@ -0,0 +1,69 @@
+#include "afk/script/StateMachine.hpp"
+
+using Afk::StateMachine;
+using Afk::StateMachineBuilder;
+
+auto StateMachineBuilder::in(std::string state) -> StateMachineBuilder & {
+  this->current_state = state;
+  return *this;
+}
+auto StateMachineBuilder::on_enter(luabridge::LuaRef callback) -> StateMachineBuilder & {
+  this->machine.on_enter[this->current_state] = callback;
+  return *this;
+}
+auto StateMachineBuilder::on_exit(luabridge::LuaRef callback) -> StateMachineBuilder & {
+  this->machine.on_exit[this->current_state] = callback;
+  return *this;
+}
+auto StateMachineBuilder::on(std::string command) -> StateMachineBuilder & {
+  this->current_command = command;
+  return *this;
+}
+auto StateMachineBuilder::go(std::string state) -> StateMachineBuilder & {
+  this->machine.transition_map[{this->current_state, this->current_command}].end_state = state;
+  return *this;
+}
+auto StateMachineBuilder::call(luabridge::LuaRef callback) -> StateMachineBuilder & {
+  this->machine.transition_map[{this->current_state, this->current_command}].callback = callback;
+  return *this;
+}
+auto StateMachineBuilder::initial_state(std::string state) -> StateMachineBuilder & {
+  this->machine.current_state = state;
+  return *this;
+}
+auto StateMachineBuilder::build() -> StateMachine {
+  return this->machine;
+}
+
+auto StateMachine::fire(std::string command) -> void {
+  auto transition = this->transition_map.find( // asdf
+      StateMachine::TransitionBaseState{this->current_state, command});
+  if (transition != this->transition_map.end()) {
+    auto exit_callback  = this->on_exit.find(this->current_state);
+    this->current_state = transition->second.end_state;
+    auto enter_callback = this->on_enter.find(this->current_state);
+    if (exit_callback != this->on_exit.end()) {
+      exit_callback->second();
+    }
+    if (enter_callback != this->on_enter.end()) {
+      enter_callback->second();
+    }
+    if (transition->second.callback) {
+      transition->second.callback();
+    }
+  } else {
+    throw std::runtime_error{"No transition was defined for receiving " +
+                             command + " for state " + this->current_state};
+  }
+}
+
+auto StateMachine::TransitionBaseState::operator<(const TransitionBaseState &rhs) const
+    -> bool {
+  return (this->command_name < rhs.command_name) && (this->current_state < rhs.current_state);
+}
+
+auto StateMachine::TransitionBaseState::operator==(const TransitionBaseState &rhs) const
+    -> bool {
+  return (this->command_name == rhs.command_name) &&
+         (this->current_state == rhs.current_state);
+}
--- src/afk/script/StateMachine.hpp
+++ src/afk/script/StateMachine.hpp
@@ -1,54 +1,72 @@
 #pragma once
+#include <functional>
+#include <map>
 #include <string>
-#include <unordered_map>
+#include <vector>
+// nomove
+#include "afk/script/LuaInclude.hpp"
+// nomove
+#include <LuaBridge/LuaBridge.h>
 
-class LuaRef {};
 namespace Afk {
   /**
-   * @brief State Machine class for Lua.
+   * @brief Finite state machine
+   */
+  class StateMachine {
+  public:
+    StateMachine() = default;
+    auto fire(std::string command) -> void;
+
+  private:
+    typedef std::function<void(void)> StateChangeCallback;
+
+    friend class StateMachineBuilder;
+    std::string current_state;
+    struct TransitionBaseState {
+      TransitionBaseState()     = default;
+      std::string current_state = {};
+      std::string command_name  = {};
+      auto operator<(const TransitionBaseState &rhs) const -> bool;
+      auto operator==(const TransitionBaseState &rhs) const -> bool;
+    };
+    struct TransitionGoal {
+      TransitionGoal()             = default;
+      std::string end_state        = {};
+      StateChangeCallback callback = {};
+    };
+    std::map<TransitionBaseState, TransitionGoal> transition_map = {};
+    std::map<std::string, StateChangeCallback> on_enter          = {};
+    std::map<std::string, StateChangeCallback> on_exit           = {};
+  };
+
+  /**
+   * @brief State Machine builder for Lua.
    * Define states with fluent-style syntax.
    * Example:
-   * fsm.in("begin").on("start").go("started").exec(startup)
+   * fsm.in("begin").on("start").go("started").call(startup)
    * fsm.in("start").on("sleep").go("sleeping").on("power_off").go("end")
    * fsm.init("begin")
    * fsm.call("start")
+   *
+   * Inspired by https://github.com/appccelerate/statemachine
    */
   class StateMachineBuilder {
   public:
-    class StateMachine;
-    StateMachineBuilder();
+    StateMachineBuilder() = default;
     auto in(std::string state) -> StateMachineBuilder &;
-    auto on_enter(LuaRef callback) -> StateMachineBuilder &;
-    auto on_exit(LuaRef callback) -> StateMachineBuilder &;
+    auto on_enter(luabridge::LuaRef callback) -> StateMachineBuilder &;
+    auto on_exit(luabridge::LuaRef callback) -> StateMachineBuilder &;
     auto on(std::string command) -> StateMachineBuilder &;
     auto go(std::string state) -> StateMachineBuilder &;
-    auto exec(LuaRef callback) -> StateMachineBuilder &;
+    auto call(luabridge::LuaRef callback) -> StateMachineBuilder &;
 
     auto initial_state(std::string state) -> StateMachineBuilder &;
 
     auto build() -> StateMachine;
 
-    class StateMachine {
-    public:
-      auto fire(std::string command) -> void;
-
-    private:
-      struct TransitionBaseState {
-        std::string current_state;
-        std::string command_name;
-        auto operator<(const TransitionBaseState &rhs) -> bool;
-      };
-      struct TransitionGoal {
-        std::string end_state;
-        LuaRef callback;
-      };
-      std::unordered_map<TransitionBaseState, TransitionGoal> transition_map;
-      std::unordered_map<std::string, LuaRef> on_enter;
-      std::unordered_map<std::string, LuaRef> on_exit;
-    };
-
   private:
-    std::string current_state;
-    std::string current_command;
+    StateMachine machine        = {};
+    std::string current_state   = {};
+    std::string current_command = {};
   };
 }
