b3ae5416d11676c9bd5c1edc7971947aec7550dc
jammy-dodgers <emily@jambox.cc>
2020-06-07 20:05:10 +0800

    place on nearest polygon

--- src/afk/component/AgentComponent.cpp
+++ src/afk/component/AgentComponent.cpp
@@ -5,7 +5,15 @@ using Afk::AI::AgentComponent;
 
 AgentComponent::AgentComponent(GameObject e, const glm::vec3 &pos, dtCrowdAgentParams &p) {
   this->owning_entity = e;
-  this->id = Afk::Engine::get().crowds.current_crowd().addAgent(&(pos.x), &p);
+  auto query = Afk::Engine::get().crowds.current_crowd().getNavMeshQuery();
+  const auto extents = glm::vec3{10.f, 10.f, 10.f};
+  const auto filter  = dtQueryFilter{};
+
+  dtPolyRef nearest_poly = {};
+  auto nearest_pos       = glm::vec3{0, 0, 0};
+  auto query_result      = query->findNearestPoly(&pos.x, &extents.x, &filter,
+                                             &nearest_poly, &nearest_pos.x);
+  this->id = Afk::Engine::get().crowds.current_crowd().addAgent(&nearest_pos.x, &p);
 }
 AgentComponent::~AgentComponent() {
   Afk::Engine::get().crowds.current_crowd().removeAgent(this->id);
