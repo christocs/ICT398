9e90b2a29864d64e10163b3bbb2d9abc54fb6ea8
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2020-06-15 19:40:06 +0800

    prevent making copies of items in entt views

--- src/afk/renderer/ModelRenderSystem.cpp
+++ src/afk/renderer/ModelRenderSystem.cpp
@@ -7,8 +7,8 @@ auto Afk::queue_models(entt::registry *registry, Afk::Renderer *renderer) -> voi
   auto render_view = registry->view<Afk::Transform, Afk::ModelSource>();
 
   for (const auto entity : render_view) {
-    const auto model_component = render_view.get<Afk::ModelSource>(entity);
-    const auto model_transform = render_view.get<Afk::Transform>(entity);
+    const auto& model_component = render_view.get<Afk::ModelSource>(entity);
+    const auto& model_transform = render_view.get<Afk::Transform>(entity);
     renderer->queue_draw({model_component.name, model_component.shader_program_path, model_transform});
   }
 }
