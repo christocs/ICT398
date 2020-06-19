30ec50961c784d4d665aeec84a689bd4611ed13a
Christo Stephenson <christo@iinet.net.au>
2020-06-19 14:39:27 +0800

    move implementation of functions from header to source file

--- src/afk/ai/NavMeshManager.cpp
+++ src/afk/ai/NavMeshManager.cpp
@@ -714,3 +714,32 @@ unsigned char *NavMeshManager::build_tile_nav_mesh(
   data_size = nav_data_size;
   return nav_data;
 }
+
+inline unsigned int NavMeshManager::nextPow2(unsigned int v) {
+  v--;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  v++;
+  return v;
+}
+
+inline unsigned int NavMeshManager::ilog2(unsigned int v) {
+  unsigned int r;
+  unsigned int shift;
+  r = (v > 0xffff) << 4;
+  v >>= r;
+  shift = (v > 0xff) << 3;
+  v >>= shift;
+  r |= shift;
+  shift = (v > 0xf) << 2;
+  v >>= shift;
+  r |= shift;
+  shift = (v > 0x3) << 1;
+  v >>= shift;
+  r |= shift;
+  r |= (v >> 1);
+  return r;
+}
