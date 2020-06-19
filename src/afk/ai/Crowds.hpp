8a772c085444a0dcd596d6032257e668f092435f
jammy-dodgers <emily@jambox.cc>
2020-06-19 13:21:46 +0800

    doxygen crowds and agentcomponent

--- src/afk/ai/Crowds.hpp
+++ src/afk/ai/Crowds.hpp
@@ -11,6 +11,9 @@
 
 namespace Afk {
   namespace AI {
+    /**
+     * AI Agent Crowd Management
+     */
     class Crowds {
     public:
       typedef int AgentID;
@@ -20,13 +23,30 @@ namespace Afk {
       Crowds(Crowds &&other) = delete;
       auto operator=(Crowds &other) -> Crowds & = delete;
       auto operator=(Crowds &&other) -> Crowds & = delete;
-
+      /**
+       * Update with delta time in seconds
+       */
       auto update(float dt_seconds) -> void;
+      /**
+       * Initialize nav mesh
+       */
       auto init(NavMeshManager::nav_mesh_ptr nav_mesh) -> void;
+      /**
+       * Get current crowd
+       * \todo façade
+       */
       auto current_crowd() -> dtCrowd &;
-
+      /**
+       * Find nearest position on the nav mesh with dist search_dist
+       * \param req requested position
+       * \param search_dist searches square in each search_dist direction
+       * \return optional containing the found position, or nothing
+       */
       auto nearest_pos(glm::vec3 req, float search_dist = 10.f)
           -> std::optional<glm::vec3>;
+      /**
+       * Move an agent
+       */
       auto request_move(AgentID id, glm::vec3 pos, float search_dist = 10.f) -> void;
 
     private:
