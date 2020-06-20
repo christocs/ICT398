e1d7770e749b42f25796300fad881fd4a68d49eb
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2020-06-20 12:45:47 +0800

    hud tidyup

--- script/component/health.lua
+++ script/component/health.lua
@@ -28,9 +28,13 @@ function health_display()
 end
 
 function round(x, decimals)
-    local n = 10^(decimals or 0)
+    local n = 10 ^ (decimals or 0)
     x = x * n
-    if x >= 0 then x = math.floor(x + 0.5) else x = math.ceil(x - 0.5) end
+    if x >= 0 then
+        x = math.floor(x + 0.5)
+    else
+        x = math.ceil(x - 0.5)
+    end
     return x / n
 end
 
