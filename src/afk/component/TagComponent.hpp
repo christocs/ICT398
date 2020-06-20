25c7779cdddfbe6ffe658a0c9ef36b8ecd5d5d66
jammy-dodgers <emily@jambox.cc>
2020-06-20 11:38:32 +0800

    Doc events and tags

--- src/afk/component/TagComponent.hpp
+++ src/afk/component/TagComponent.hpp
@@ -5,8 +5,15 @@
 namespace Afk {
   class TagComponent : public BaseComponent {
   public:
+    /**
+     * Tag type
+     * \todo change to enum class
+     */
     enum Tag { TERRAIN, PLAYER, ENEMY };
-
+    /**
+     * Set of tags on this object
+     * \todo change to unordered_set
+     */
     std::set<Afk::TagComponent::Tag> tags = {};
   };
 }
