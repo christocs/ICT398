8c1a1a3df23b870e1370f55065dde3c4f9625a4a
jammy-dodgers <emily@jambox.cc>
2020-09-16 14:27:16 +0800

    Add missing emotion

--- src/afk/component/EmotionComponent.hpp
+++ src/afk/component/EmotionComponent.hpp
@@ -4,21 +4,21 @@
 namespace afk {
   /**
    * Emotions component
-   * 
+   *
    * The concept of emotion is applicable to all evolutionary levels and applies to animals as well as to humans.
    * Emotions have an evolutionary history and have evolved various forms of expression in different species.
    * Emotions served an adaptive role in helping organisms deal with key survival issues posed by the environment.
-   * Despite different forms of expression of emotions in different species, there are certain common elements, 
+   * Despite different forms of expression of emotions in different species, there are certain common elements,
    *    or prototype patterns, that can be identified.
    * There is a small number of basic, primary, or prototype emotions.
-   * All other emotions are mixed or derivative states; that is, they occur as combinations, mixtures, or compounds 
+   * All other emotions are mixed or derivative states; that is, they occur as combinations, mixtures, or compounds
    *    of the primary emotions.
-   * Primary emotions are hypothethical constructs or idealized states whose properties and characteristics can only 
+   * Primary emotions are hypothethical constructs or idealized states whose properties and characteristics can only
    *    be inferred from various kinds of evidence.
    * Primary emotions can be conceptualized in terms of pairs of polar opposites.
    * All emotions vary in their degree of similarity to one another.
-   * Each emotion can exist in varying degrees of intensity or levels of arousal. 
-   * 
+   * Each emotion can exist in varying degrees of intensity or levels of arousal.
+   *
    * (https://en.wikipedia.org/wiki/Robert_Plutchik)
    */
   class EmotionComponent : public BaseComponent {
@@ -27,37 +27,43 @@ namespace afk {
      * Ecstasy - Joy - Serenity
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Serenity;
+    float serenity;
     /**
      * Admiration - Trust - Acceptance
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Acceptance;
+    float acceptance;
     /**
      * Terror - Fear - Apprehension
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Apprehension;
+    float apprehension;
     /**
      * Amazement - Surprise - Distraction
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Distraction;
+    float distraction;
     /**
      * Grief - Sadness - Pensiveness
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Pensiveness;
+    float pensiveness;
     /**
      * Loathing - Disgust - Boredom
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Boredom;
+    float boredom;
     /**
      * Rage - Anger - Annoyance
      * Value from 0 (none of this emotion) to 1 (all of this emotion)
      */
-    float Annoyance;
+    float annoyance;
+
+    /**
+     * Interest - Anticipation - Vigilance
+     * Value from 0 (none of this emotion) to 1 (all of this emotion)
+     */
+    float interest;
 
     /**
      * Constructor
