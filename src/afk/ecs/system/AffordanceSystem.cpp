b08ec23b4dc0b5d2a1d7a28e2e03159bea7f5bc4
jammy-dodgers <emily@jambox.cc>
2020-10-26 19:39:12 +0800

    Switch to std::max_element

--- src/afk/ecs/system/AffordanceSystem.cpp
+++ src/afk/ecs/system/AffordanceSystem.cpp
@@ -1,5 +1,7 @@
 #include "afk/ecs/system/AffordanceSystem.hpp"
 
+#include <algorithm>
+
 #include "afk/Engine.hpp"
 #include "afk/ecs/component/AffordanceComponent.hpp"
 #include "afk/ecs/component/NeedsComponent.hpp"
@@ -15,7 +17,15 @@ auto AffordanceSystem::update() -> void {
   auto &registry  = afk.ecs.registry;
   const auto view = registry.view<NeedsComponent, PositionComponent>();
   for (const auto entity : view) {
-    auto &needs = registry.get<NeedsComponent>(entity);
-    auto &pos   = registry.get<PositionComponent>(entity);
+    auto &needs          = registry.get<NeedsComponent>(entity);
+    auto &pos            = registry.get<PositionComponent>(entity);
+    entt::entity nearest = entt::null;
+    auto greatest_need =
+        std::max_element(needs.needs.need.begin(), needs.needs.need.end(),
+                         [](auto a, auto b) { return a.second < b.second; });
+    const auto affordables_view =
+        registry.view<AffordanceComponent, PositionComponent>();
+    for (const auto affordable_ents : affordables_view) {
+    }
   }
 }
