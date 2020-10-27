21357b7dc2ea898128e6a4e337c25cba178d3213
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2020-10-27 13:59:48 +0800

    allow a scene to have no components defined for a prefab

--- src/afk/scene/SceneManager.cpp
+++ src/afk/scene/SceneManager.cpp
@@ -52,53 +52,55 @@ auto SceneManager::load_scenes_from_dir(const path &dir_path) -> void {
       auto prefab =
           afk.prefab_manager.prefab_map.at(entity_json.at("name").get<string>());
 
-      const auto components_json = entity_json.at("components");
+      if (entity_json.count("components") == 1) {
 
-      for (const auto &[component_name, component_json] : components_json.items()) {
-        const auto &j  = component_json;
-        auto component = PrefabManager::COMPONENT_MAP.at(component_name);
+        const auto components_json = entity_json.at("components");
 
-        auto visitor = Visitor{
-            [j](ModelsComponent &c) { c = j.get<ModelsComponent>(); },
-            [j](TransformComponent &c) { c = j.get<TransformComponent>(); },
-            [j](ColliderComponent &c) { c = j.get<ColliderComponent>(); },
-            [j, components_json_ref = std::ref(components_json), &prefab,
-             &afk](PhysicsComponent &c) {
-              c = j.get<PhysicsComponent>();
+        for (const auto &[component_name, component_json] : components_json.items()) {
+          const auto &j  = component_json;
+          auto component = PrefabManager::COMPONENT_MAP.at(component_name);
 
-              const auto components_json = components_json_ref.get();
+          auto visitor = Visitor{
+              [j](ModelsComponent &c) { c = j.get<ModelsComponent>(); },
+              [j](TransformComponent &c) { c = j.get<TransformComponent>(); },
+              [j](ColliderComponent &c) { c = j.get<ColliderComponent>(); },
+              [j, components_json_ref = std::ref(components_json), &prefab,
+               &afk](PhysicsComponent &c) {
+                c = j.get<PhysicsComponent>();
 
-              // no need to do checks if components are missing, as all prefabs already enforce these checks
-              // here we are just overwriting prefab components if they are defined
+                const auto components_json = components_json_ref.get();
 
-              auto collider  = ColliderComponent{};
-              auto transform = TransformComponent{};
+                // no need to do checks if components are missing, as all prefabs already enforce these checks
+                // here we are just overwriting prefab components if they are defined
 
-              // if the scene defines the collider component, use the one in the scene, else use the one provided by the prefab
-              if (components_json_ref.get().count("Collider") == 1) {
-                collider = components_json_ref.get().at("Collider").get<ColliderComponent>();
-              } else {
-                collider = std::get<ColliderComponent>(
-                    prefab.components.at("Collider"));
-              }
+                auto collider  = ColliderComponent{};
+                auto transform = TransformComponent{};
 
-              // if the scene defines the transform component, use the one in the scene, else use the one provided by the prefab
-              if (components_json_ref.get().count("Transform") == 1) {
-                transform =
-                    components_json_ref.get().at("Transform").get<TransformComponent>();
-              } else {
-                transform = std::get<TransformComponent>(prefab.components.at("Collider"));
-              }
+                // if the scene defines the collider component, use the one in the scene, else use the one provided by the prefab
+                if (components_json_ref.get().count("Collider") == 1) {
+                  collider = components_json_ref.get().at("Collider").get<ColliderComponent>();
+                } else {
+                  collider = std::get<ColliderComponent>(prefab.components.at("Collider"));
+                }
 
-              afk.physics_system.initialize_physics_component(c, collider, transform);
-            },
-            [](auto) { afk_unreachable(); }};
+                // if the scene defines the transform component, use the one in the scene, else use the one provided by the prefab
+                if (components_json_ref.get().count("Transform") == 1) {
+                  transform =
+                      components_json_ref.get().at("Transform").get<TransformComponent>();
+                } else {
+                  transform = std::get<TransformComponent>(prefab.components.at("Collider"));
+                }
 
-        std::visit(visitor, component);
+                afk.physics_system.initialize_physics_component(c, collider, transform);
+              },
+              [](auto) { afk_unreachable(); }};
 
-        afk_assert(prefab.components.find(component_name) != prefab.components.end(),
-                   "Prefab missing component in entity");
-        prefab.components[component_name] = component;
+          std::visit(visitor, component);
+
+          afk_assert(prefab.components.find(component_name) != prefab.components.end(),
+                     "Prefab missing component in entity");
+          prefab.components[component_name] = component;
+        }
       }
 
       scene.prefabs.push_back(prefab);
